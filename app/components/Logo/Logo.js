d40e5d10cc633b06129ab3ce2a01cb1b814a309e
Ojini Chizoba Jude <judoc96@gmail.com>
2020-02-01 15:39:19 +0100

    animating the logo

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,22 +1,92 @@
-import React from 'react';
-import {View, Image, ImageBackground, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  ImageBackground,
+  Text,
+  Keyboard,
+  Animated,
+  Platform,
+} from 'react-native';
 
-import styles from './styles';
+import styles, {imageWidth} from './styles';
 
-const Logo = () => (
-  <View style={styles.container}>
-    <ImageBackground
-      resizeMode="contain"
-      source={require('./images/background.png')}
-      style={styles.bgImg}>
-      <Image
-        resizeMode="contain"
-        style={styles.img}
-        source={require('./images/logo.png')}
-      />
-    </ImageBackground>
-    <Text style={styles.text}>Currency Converter</Text>
-  </View>
-);
+const Logo = () => {
+  const largeContainerWidth = new Animated.Value(imageWidth);
+  const largeImageWidth = new Animated.Value(imageWidth / 2);
+  const ANIMATION_DURATION = 250;
+
+  const keyboardShow = () => {
+    Animated.parallel([
+      Animated.timing(largeContainerWidth, {
+        toValue: imageWidth / 2,
+        duration: ANIMATION_DURATION,
+      }),
+      Animated.timing(largeImageWidth, {
+        toValue: imageWidth / 4,
+        duration: ANIMATION_DURATION,
+      }),
+    ]).start();
+  };
+
+  const keyboardHide = () => {
+    Animated.parallel([
+      Animated.timing(largeContainerWidth, {
+        toValue: imageWidth,
+        duration: ANIMATION_DURATION,
+      }),
+      Animated.timing(largeImageWidth, {
+        toValue: imageWidth / 2,
+        duration: ANIMATION_DURATION,
+      }),
+    ]).start();
+  };
+
+  useEffect(() => {
+    let keyboardShowing = 'keyboardWillShow';
+    let keyboardHiden = 'keyboardWillHide';
+
+    if (Platform.OS === 'android') {
+      keyboardShowing = 'keyboardDidShow';
+      keyboardHiden = 'keyboardDidHide';
+    }
+
+    let keyboardDidShowListener = Keyboard.addListener(
+      keyboardShowing,
+      keyboardShow,
+    );
+
+    let keyboardDidHideListener = Keyboard.addListener(
+      keyboardHiden,
+      keyboardHide,
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.imgWrapper,
+          {width: largeContainerWidth, height: largeContainerWidth},
+        ]}>
+        <ImageBackground
+          resizeMode="contain"
+          source={require('./images/background.png')}
+          style={styles.bgImg}>
+          <Animated.Image
+            resizeMode="contain"
+            style={[styles.img, {width: largeImageWidth}]}
+            source={require('./images/logo.png')}
+          />
+        </ImageBackground>
+      </Animated.View>
+      <Text style={styles.text}>Currency Converter</Text>
+    </View>
+  );
+};
 
 export default Logo;
--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -1,26 +1,30 @@
 import {StyleSheet, Dimensions} from 'react-native';
 
-let imageWidth = Dimensions.get('window').width / 2;
+export let imageWidth = Dimensions.get('window').width / 2;
 
 export default StyleSheet.create({
   container: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
-  bgImg: {
+  imgWrapper: {
     width: imageWidth,
     height: imageWidth,
+  },
+  bgImg: {
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
   img: {
-    width: imageWidth / 2
+    width: imageWidth / 2,
   },
   text: {
     color: '#fff',
     fontWeight: '600',
     fontSize: 28,
     marginTop: 15,
-    letterSpacing: -0.5
+    letterSpacing: -0.5,
   },
 });
